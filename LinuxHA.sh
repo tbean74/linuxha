e00c5d95a037cc9ddbf6ce7af40fdecc2d8d7866
Travis Bean <tbean74@gmail.com>
2025-10-10 03:10:00 +0000

    Updated Postfix configuration.

--- LinuxHA.sh
+++ LinuxHA.sh
@@ -4193,21 +4193,55 @@ EOF.main.cf
 if [ ! -f /etc/postfix/master.cf.orig ]; then 
   cp /etc/postfix/master.cf /etc/postfix/master.cf.orig
 fi
-sed -i "/submission/ s|#||
-  /smtps/ s|#||
-  /syslog_name=/ s|#||
-  /smtpd_tls_security_level=/ s|#|| 
-  /smtpd_tls_wrappermode=/ s|#||
-  /smtpd_reject_unlisted_recipient=/ s|#||
-  /smtpd_relay_restrictions=/ s|#||
-  /milter_macro_daemon_name=/ s|#||" /etc/postfix/master.cf
-
-# Update master.cf for Dovecot's Vmail support.
-grep -q "dovecot" /etc/postfix/master.cf || \
-cat >> /etc/postfix/master.cf << EOF.master.cf
+cat > /etc/postfix/master.cf << EOF.master.cf
+smtp      inet  n       -       y       -       -       smtpd
+submission inet n       -       y       -       -       smtpd
+  -o syslog_name=postfix/submission
+  -o smtpd_tls_security_level=encrypt
+  -o smtpd_sasl_auth_enable=yes
+  -o smtpd_tls_auth_only=yes
+  -o smtpd_reject_unlisted_recipient=no
+  -o milter_macro_daemon_name=ORIGINATING
+pickup    unix  n       -       y       60      1       pickup
+cleanup   unix  n       -       y       -       0       cleanup
+qmgr      unix  n       -       n       300     1       qmgr
+tlsmgr    unix  -       -       y       1000?   1       tlsmgr
+rewrite   unix  -       -       y       -       -       trivial-rewrite
+bounce    unix  -       -       y       -       0       bounce
+defer     unix  -       -       y       -       0       bounce
+trace     unix  -       -       y       -       0       bounce
+verify    unix  -       -       y       -       1       verify
+flush     unix  n       -       y       1000?   0       flush
+proxymap  unix  -       -       n       -       -       proxymap
+proxywrite unix -       -       n       -       1       proxymap
+smtp      unix  -       -       y       -       -       smtp
+relay     unix  -       -       y       -       -       smtp
+  -o syslog_name=postfix/$service_name
+showq     unix  n       -       y       -       -       showq
+error     unix  -       -       y       -       -       error
+retry     unix  -       -       y       -       -       error
+discard   unix  -       -       y       -       -       discard
+local     unix  -       n       n       -       -       local
+virtual   unix  -       n       n       -       -       virtual
+lmtp      unix  -       -       y       -       -       lmtp
+anvil     unix  -       -       y       -       1       anvil
+scache    unix  -       -       y       -       1       scache
+postlog   unix-dgram n  -       n       -       1       postlogd
+maildrop  unix  -       n       n       -       -       pipe
+  flags=DRXhu user=vmail argv=/usr/bin/maildrop -d ${recipient}
+uucp      unix  -       n       n       -       -       pipe
+  flags=Fqhu user=uucp argv=uux -r -n -z -a$sender - $nexthop!rmail ($recipient)
+ifmail    unix  -       n       n       -       -       pipe
+  flags=F user=ftn argv=/usr/lib/ifmail/ifmail -r $nexthop ($recipient)
+bsmtp     unix  -       n       n       -       -       pipe
+  flags=Fq. user=bsmtp argv=/usr/lib/bsmtp/bsmtp -t$nexthop -f$sender $recipient
+scalemail-backend unix -       n       n       -       2       pipe
+  flags=R user=scalemail argv=/usr/lib/scalemail/bin/scalemail-store ${nexthop} ${user} ${extension}
+mailman   unix  -       n       n       -       -       pipe
+  flags=FRX user=list argv=/usr/lib/mailman/bin/postfix-to-mailman.py ${nexthop} ${user
 dovecot   unix  -       n       n       -       -       pipe
-     flags=DRhu user=vmail:vmail argv=/usr/lib/dovecot/deliver \
--d \${recipient} -f \${sender}
+  flags=DRhu user=vmail:vmail argv=/usr/lib/dovecot/deliver \
+  -d \${recipient} -f \${sender}
 EOF.master.cf
 
 # Create the root alias and update the local alias database.
